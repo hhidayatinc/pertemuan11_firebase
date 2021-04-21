d0398fa1cf586f9f3ff477dd29e621d7fd89076b
hhidayatinc <chasanah55@gmail.com>
2021-04-21 18:16:02 +0700

    Create login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase/pages/sign_in.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'first_screen.dart';
 
@@ -9,32 +10,123 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final kPrimaryColor = Color(0xFF6F35A5);
+  final kPrimaryLightColor = Color(0xFFF1E6FF);
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlutterLogo(size: 150),
-              SizedBox(height: 50),
-              _signInButton(),
-            ],
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(40, 60, 40, 40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        
+        children: <Widget>[
+          Text(
+            "LOGIN",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: size.height * 0.03),
+          SvgPicture.asset(
+            "icons/login.svg",
+            height: 150,
+          ),
+          //SizedBox(height: size.height * 0.03),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            width: size.width * 0.8,
+            decoration: BoxDecoration(
+              color: kPrimaryLightColor,
+              borderRadius: BorderRadius.circular(29),
+            ),
+            child: TextField(
+              cursorColor: kPrimaryColor,
+              decoration: InputDecoration(
+                icon: Icon(Icons.people, color: kPrimaryColor),
+                hintText: "Username",
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            width: size.width * 0.8,
+            decoration: BoxDecoration(
+              color: kPrimaryLightColor,
+              borderRadius: BorderRadius.circular(29),
+            ),
+            child: TextField(
+              obscureText: true,
+              cursorColor: kPrimaryColor,
+              decoration: InputDecoration(
+                hintText: "Password",
+                icon: Icon(
+                  Icons.lock,
+                  color: kPrimaryColor,
+                ),
+                suffixIcon: Icon(
+                  Icons.visibility,
+                  color: kPrimaryColor,
+                ),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          Container(
+             margin: EdgeInsets.symmetric(vertical: 10),
+      width: size.width * 0.8,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(29),
+        child: FlatButton(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+          color: kPrimaryColor,
+          onPressed: (){},
+          child: Text(
+            "Login",
+            style: TextStyle(color: Colors.white),
           ),
         ),
       ),
-    );
-  }
-
-  Widget _signInButton() {
-    // ignore: deprecated_member_use
-    return OutlineButton(
-      splashColor: Colors.grey,
-      onPressed: () {
-        signInWithGoogle().then((result){
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: size.height * 0.02),
+      width: size.width * 0.8,
+      child: Row(
+        children: <Widget>[
+        //   Divider(
+        //     color: Color(0xFFD9D9D9),
+        // height: 1.5,
+        //   ),
+          Padding(
+            padding: const EdgeInsets.only(left: 130),
+            child: Text(
+              "OR",
+              style: TextStyle(
+                color: kPrimaryColor,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        //   Divider(
+        //     color: Color(0xFFD9D9D9),
+        // height: 1.5,
+        //   ),
+        ],
+      ),
+          ),
+          Container(
+             margin: EdgeInsets.symmetric(vertical: 4),
+      width: size.width * 0.8,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(29),
+        child: FlatButton(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+          color: kPrimaryColor,
+          onPressed: (){
+            signInWithGoogle().then((result){
           if (result != null) {
             Navigator.of(context).push(
               MaterialPageRoute (
@@ -45,30 +137,27 @@ class _LoginPageState extends State<LoginPage> {
             );
           }
         });
-      },
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-      highlightElevation: 0,
-      borderSide: BorderSide(color: Colors.grey),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Text(
-                'Sign in with Google',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.grey,
-                ),
-              ),
-            )
-          ],
+          },
+          child: Text(
+            "Sign in With Google",
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       ),
-    );
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, top: 10),
+            child: Text(
+              "Don't have account? Register",
+              style: TextStyle(
+                color: kPrimaryColor,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
+  
 }
